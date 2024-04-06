aaac65acd5caf4802186c98b1bd1bfe55409db64
Erick Michel <erick.michel2002@gmail.com>
2024-04-06 14:18:47 -0300

    Instalando laravel jetstream para autenticaçao de usuario.

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\ShowTweets;
 
-Route::get('tweets', ShowTweets::class);
+Route::get('tweets', ShowTweets::class)->middleware(['auth']);
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
